0f6cb61dbbb4bef033035d92be8fd6f98b31eebd
paumanri <p.manresabad@gmail.com>
2025-04-09 21:15:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-    new DataTable(document.querySelector('table'));
+    const table = new DataTable('#taula-elements', {
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/ca.json'
+        },
+        paging: true,
+        searching: true,
+        ordering: true,
+        pageLength: 5,
+        lengthMenu: [5, 10, 25, 50, 100],
+        initComplete: function () {
+            // Filtres per columna (a <tfoot>)
+            const api = this.api();
+            api.columns().every(function () {
+                const column = this;
+                const input = column.footer().querySelector('input');
+                if (input) {
+                    input.addEventListener('keyup', function () {
+                        column.search(this.value).draw();
+                    });
+                }
+            });
+        }
+    });
 });
