070b68eb740b9db1349d7d2c9d0c276a2c367e63
paumanri <p.manresabad@gmail.com>
2025-04-09 21:06:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Inicialitza DataTables a la taula
-    const taula = document.querySelector('table');
-    if (taula) {
-        new DataTable(taula);
-    }
+    new DataTable(document.querySelector('table'));
 });
